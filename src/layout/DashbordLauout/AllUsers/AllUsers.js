96e8ffbf0b6c6491ee35e283d2fa5ca08461e202
unknown <samdshakilofficial@gmail.com>
2022-11-19 06:26:18 +0500

    add users table and Local link

--- src/layout/DashbordLauout/AllUsers/AllUsers.js
+++ src/layout/DashbordLauout/AllUsers/AllUsers.js
@@ -1,11 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const AllUsers = () => {
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/users`);
+      const data = await res.json();
+      return data;
+    },
+  });
   return (
     <div>
-      <h3 className="text-5xl bond-bold text-white">
-        All lllllllllllllllllllllllllUser
-      </h3>
+      <h3 className="text-5xl bond-bold text-white">All User</h3>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, i) => (
+              <tr key={user._id}>
+                <th>{i + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button className="btn btn-sx btn-primary">Make Admin</button>
+                </td>
+                <td>
+                  <button className="btn btn-xs btn-danger">delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
